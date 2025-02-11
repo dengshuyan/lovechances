74492a81abe17bc44a1fef82ee470a3c8f13a0bc
Alina Deng <shuyan803@gmail.com>
2025-02-11 17:10:25 -0500

    Fixed radio buttons

--- src/components/SingleSelect.jsx
+++ src/components/SingleSelect.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Radio } from "@mui/material";
 
 export default function SingleSelect({ title, options, value, onChange }) {
   return (
@@ -14,6 +14,17 @@ export default function SingleSelect({ title, options, value, onChange }) {
               ${value === option ? 'border-black bg-gray-50' : 'border-gray-200 hover:border-gray-300'}`}
           >
             <Typography variant="body1">{option}</Typography>
+            <Radio 
+              checked={value === option}
+              onChange={() => onChange(option)}
+              sx={(theme) => ({
+                color: theme.palette.grey[300],
+                '&.Mui-checked': {
+                  color: theme.palette.primary.main,
+                },
+                padding: 0
+              })}
+            />
           </div>
         ))}
       </div>
